7e20c34777732998bf31e43c6f4bd6870fe7326a
siam666-shell <abdullahsiam009@gmail.com>
2022-11-18 22:23:30 +0600

    final

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const data = require("./db.js");
+const jsonServer = require('json-server');
 
-const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router(data);
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
 
+//Bind the router db to the app
+server.db = router.db;
 server.use(middlewares);
-server.use(router);
 
-server.listen(port);
+const port = process.env.PORT || 9000;
+server.use(router);
+server.listen(port, () => {
+  console.log(`server running on port: ${port}`);
+});
